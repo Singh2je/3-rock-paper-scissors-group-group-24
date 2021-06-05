c7c17739c8b2e5a49bfe77e922c9aa05d02b0139
Jaspreet Singh <71667958+Singh2je@users.noreply.github.com>
2021-06-04 23:19:40 -0400

    commented out unncessary lines.

--- tests/5.index.test.js
+++ tests/5.index.test.js
@@ -30,20 +30,21 @@ describe(`Testing updateScoreTally `, function () {
     jest.resetModules();
   });
 
-  test(`divs immediately under the forms should have a class name of form-group `, function () {
-    const welcomeFormDiv = document.querySelector(`#welcome-screen > form > div`);
-    const gameFormDiv = document.querySelector(`#game-screen > form > div`);
+  // test(`divs immediately under the forms should have a class name of form-group `, function () {
+  //   const welcomeFormDiv = document.querySelector(`#welcome-screen > form > div`);
+  //   const gameFormDiv = document.querySelector(`#game-screen > form > div`);
 
-    expect(welcomeFormDiv).toHaveClass(`form-group`);
-    expect(gameFormDiv).toHaveClass(`form-group`);
-  });
+  //   expect(welcomeFormDiv).toHaveClass(`form-group`);
+  //   expect(gameFormDiv).toHaveClass(`form-group`);
+  // });
 
-  it(`renders the game screen via JavaScript when the button is clicked`, async () => {
+  it(`Check to make sure UpdateScoreTally function starts the score at zero for both users`, async () => {
     const scoreParagraph = document.querySelector(`#score`);
     const startGameButton = getByText(container, /start game!/i);
     _document.addEventListener(`DOMContentLoaded`, () => {
       fireEvent.click(startGameButton);
 
+      // We can try adding a user name and making sure it gives exactly what we enter as the user name.
       //The start button is basically clicked now, everything in the start button is happening, now we can test anything that should happen 
       //that is supposed to happen after the start game button is clicked.
       
